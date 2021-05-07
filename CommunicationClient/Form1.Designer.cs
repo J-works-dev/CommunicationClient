21a6e1b804716cc75b508cd91a95f9ff8a823f82
SangJoon Lee(Jeremy) <sangjoon.jeremy.lee@gmail.com>
2021-05-07 08:52:17 +0800

    Initial Commit

---
+++ CommunicationClient/Form1.Designer.cs
@@ -0,0 +1,242 @@
+﻿
+namespace CommunicationClient
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.StatusText = new System.Windows.Forms.ToolStripStatusLabel();
+            this.buttonLogin = new System.Windows.Forms.Button();
+            this.buttonSend = new System.Windows.Forms.Button();
+            this.textBoxSend = new System.Windows.Forms.TextBox();
+            this.labelPw = new System.Windows.Forms.Label();
+            this.labelId = new System.Windows.Forms.Label();
+            this.textBoxPw = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.textBoxId = new System.Windows.Forms.TextBox();
+            this.textBoxReceived = new System.Windows.Forms.TextBox();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBoxPipeName = new System.Windows.Forms.TextBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.buttonDisconnect = new System.Windows.Forms.Button();
+            this.statusStrip1.SuspendLayout();
+            this.SuspendLayout();
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.StatusText});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 437);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(502, 22);
+            this.statusStrip1.TabIndex = 21;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // StatusText
+            // 
+            this.StatusText.Name = "StatusText";
+            this.StatusText.Size = new System.Drawing.Size(0, 16);
+            // 
+            // buttonLogin
+            // 
+            this.buttonLogin.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonLogin.Location = new System.Drawing.Point(346, 119);
+            this.buttonLogin.Name = "buttonLogin";
+            this.buttonLogin.Size = new System.Drawing.Size(129, 30);
+            this.buttonLogin.TabIndex = 20;
+            this.buttonLogin.Text = "Login";
+            this.buttonLogin.UseVisualStyleBackColor = true;
+            this.buttonLogin.Click += new System.EventHandler(this.buttonLogin_Click);
+            // 
+            // buttonSend
+            // 
+            this.buttonSend.Enabled = false;
+            this.buttonSend.Location = new System.Drawing.Point(387, 341);
+            this.buttonSend.Name = "buttonSend";
+            this.buttonSend.Size = new System.Drawing.Size(88, 70);
+            this.buttonSend.TabIndex = 19;
+            this.buttonSend.Text = "SEND";
+            this.buttonSend.UseVisualStyleBackColor = true;
+            this.buttonSend.Click += new System.EventHandler(this.buttonSend_Click);
+            // 
+            // textBoxSend
+            // 
+            this.textBoxSend.Enabled = false;
+            this.textBoxSend.Font = new System.Drawing.Font("Calibri", 10.2F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxSend.Location = new System.Drawing.Point(25, 341);
+            this.textBoxSend.Multiline = true;
+            this.textBoxSend.Name = "textBoxSend";
+            this.textBoxSend.Size = new System.Drawing.Size(356, 70);
+            this.textBoxSend.TabIndex = 18;
+            // 
+            // labelPw
+            // 
+            this.labelPw.AutoSize = true;
+            this.labelPw.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelPw.Location = new System.Drawing.Point(182, 97);
+            this.labelPw.Name = "labelPw";
+            this.labelPw.Size = new System.Drawing.Size(67, 18);
+            this.labelPw.TabIndex = 17;
+            this.labelPw.Text = "Password";
+            // 
+            // labelId
+            // 
+            this.labelId.AutoSize = true;
+            this.labelId.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.labelId.Location = new System.Drawing.Point(22, 97);
+            this.labelId.Name = "labelId";
+            this.labelId.Size = new System.Drawing.Size(21, 18);
+            this.labelId.TabIndex = 16;
+            this.labelId.Text = "ID";
+            // 
+            // textBoxPw
+            // 
+            this.textBoxPw.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPw.Location = new System.Drawing.Point(185, 119);
+            this.textBoxPw.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxPw.Name = "textBoxPw";
+            this.textBoxPw.Size = new System.Drawing.Size(154, 30);
+            this.textBoxPw.TabIndex = 15;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(22, 73);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(48, 18);
+            this.label1.TabIndex = 14;
+            this.label1.Text = "Log In:";
+            // 
+            // textBoxId
+            // 
+            this.textBoxId.Font = new System.Drawing.Font("Microsoft Sans Serif", 12F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxId.Location = new System.Drawing.Point(25, 119);
+            this.textBoxId.Margin = new System.Windows.Forms.Padding(4);
+            this.textBoxId.Name = "textBoxId";
+            this.textBoxId.Size = new System.Drawing.Size(152, 30);
+            this.textBoxId.TabIndex = 13;
+            // 
+            // textBoxReceived
+            // 
+            this.textBoxReceived.Location = new System.Drawing.Point(25, 186);
+            this.textBoxReceived.Multiline = true;
+            this.textBoxReceived.Name = "textBoxReceived";
+            this.textBoxReceived.ReadOnly = true;
+            this.textBoxReceived.ScrollBars = System.Windows.Forms.ScrollBars.Vertical;
+            this.textBoxReceived.Size = new System.Drawing.Size(450, 142);
+            this.textBoxReceived.TabIndex = 25;
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(22, 164);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(76, 17);
+            this.label3.TabIndex = 24;
+            this.label3.Text = "Messages:";
+            // 
+            // textBoxPipeName
+            // 
+            this.textBoxPipeName.Font = new System.Drawing.Font("Microsoft Sans Serif", 9F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.textBoxPipeName.Location = new System.Drawing.Point(25, 34);
+            this.textBoxPipeName.Name = "textBoxPipeName";
+            this.textBoxPipeName.Size = new System.Drawing.Size(314, 24);
+            this.textBoxPipeName.TabIndex = 23;
+            this.textBoxPipeName.Text = "\\\\.\\pipe\\myNamedPipe";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(22, 12);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(81, 17);
+            this.label2.TabIndex = 22;
+            this.label2.Text = "Pipe Name:";
+            // 
+            // buttonDisconnect
+            // 
+            this.buttonDisconnect.Font = new System.Drawing.Font("Calibri", 9F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.buttonDisconnect.Location = new System.Drawing.Point(346, 34);
+            this.buttonDisconnect.Name = "buttonDisconnect";
+            this.buttonDisconnect.Size = new System.Drawing.Size(129, 24);
+            this.buttonDisconnect.TabIndex = 26;
+            this.buttonDisconnect.Text = "Connect";
+            this.buttonDisconnect.UseVisualStyleBackColor = true;
+            this.buttonDisconnect.Click += new System.EventHandler(this.buttonDisconnect_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(502, 459);
+            this.Controls.Add(this.buttonDisconnect);
+            this.Controls.Add(this.statusStrip1);
+            this.Controls.Add(this.buttonLogin);
+            this.Controls.Add(this.buttonSend);
+            this.Controls.Add(this.textBoxSend);
+            this.Controls.Add(this.labelPw);
+            this.Controls.Add(this.labelId);
+            this.Controls.Add(this.textBoxPw);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.textBoxId);
+            this.Controls.Add(this.textBoxReceived);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.textBoxPipeName);
+            this.Controls.Add(this.label2);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+
+        #endregion
+
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel StatusText;
+        private System.Windows.Forms.Button buttonLogin;
+        private System.Windows.Forms.Button buttonSend;
+        private System.Windows.Forms.TextBox textBoxSend;
+        private System.Windows.Forms.Label labelPw;
+        private System.Windows.Forms.Label labelId;
+        private System.Windows.Forms.TextBox textBoxPw;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.TextBox textBoxId;
+        private System.Windows.Forms.TextBox textBoxReceived;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBoxPipeName;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Button buttonDisconnect;
+    }
+}
+
---
+++ CommunicationClient/Form1.cs
@@ -0,0 +1,106 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace CommunicationClient
+{
+    public partial class Form1 : Form
+    {
+        private Pipe pipeClient;
+        public Form1()
+        {
+            InitializeComponent();
+            CreateNewPipeClient();
+        }
+
+        void CreateNewPipeClient()
+        {
+            if (pipeClient != null)
+            {
+                pipeClient.MessageReceived -= pipeClient_MessageReceived;
+                pipeClient.ServerDisconnected -= pipeClient_ServerDisconnected;
+            }
+
+            pipeClient = new Pipe();
+            pipeClient.MessageReceived += pipeClient_MessageReceived;
+            pipeClient.ServerDisconnected += pipeClient_ServerDisconnected;
+        }
+
+        void pipeClient_ServerDisconnected()
+        {
+            Invoke(new Pipe.ServerDisconnectedHandler(EnableStartButton));
+        }
+
+        void EnableStartButton()
+        {
+            buttonDisconnect.Enabled = true;
+        }
+
+        void pipeClient_MessageReceived(byte[] message)
+        {
+            Invoke(new Pipe.MessageReceivedHandler(DisplayReceivedMessage),
+                new object[] { message });
+        }
+
+        void DisplayReceivedMessage(byte[] message)
+        {
+            ASCIIEncoding encoder = new ASCIIEncoding();
+            string str = encoder.GetString(message, 0, message.Length);
+
+            if (str == "close")
+            {
+                pipeClient.Disconnect();
+
+                CreateNewPipeClient();
+                pipeClient.Connect(textBoxPipeName.Text);
+            }
+            else if (str == "logged in")
+            {
+                textBoxSend.Enabled = true;
+                buttonSend.Enabled = true;
+            }
+            textBoxReceived.Text += str + "\r\n";
+        }
+
+        private void buttonDisconnect_Click(object sender, EventArgs e)
+        {
+            if (buttonDisconnect.Text == "Connect")
+            {
+                pipeClient.Connect(textBoxPipeName.Text);
+
+                if (pipeClient.Connected)
+                    buttonDisconnect.Text = "Disconnect";
+            }
+            else if (buttonDisconnect.Text == "Disconnect")
+            {
+                pipeClient.Disconnect();
+
+                if (!pipeClient.Connected)
+                    buttonDisconnect.Text = "Connect";
+            }
+        }
+
+        private void buttonLogin_Click(object sender, EventArgs e)
+        {
+            ASCIIEncoding encoder = new ASCIIEncoding();
+
+            string id = textBoxId.Text;
+            string pw = textBoxPw.Text;
+
+            pipeClient.SendMessage(encoder.GetBytes(id + "," + pw));
+        }
+
+        private void buttonSend_Click(object sender, EventArgs e)
+        {
+            ASCIIEncoding encoder = new ASCIIEncoding();
+
+            pipeClient.SendMessage(encoder.GetBytes(textBoxSend.Text));
+        }
+    }
+}
